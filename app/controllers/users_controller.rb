a889ac4f14d88b93743aae229340a03351f6c976
reomomo <reomomo31672@gmail.com>
2023-08-30 22:00:53 +0000

    出席・欠席の変更機能完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  def change
+    @user = User.find(params[:id])
+    @user.email = @user.email
+    @user.last_name = @user.last_name
+    @user.first_name = @user.first_name
+    @user.keyword = @user.keyword
+    @user.update(user_params)
+    redirect_to public_relation_path(params[:memo_id])
+  end
+
   def show
   end
 
@@ -9,5 +19,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :last_name, :first_name, :keyword, :is_participated)
   end
 end
